e0445e7e6c579870fea85e3fdf9513bf4e7d781c
HarshAnand20102005 <pixelpunk45@gmail.com>
2025-07-19 19:42:42 +0530

    Update db_connection.py

--- Backend/db_connection.py
+++ Backend/db_connection.py
@@ -3,8 +3,11 @@ import pyodbc
 def get_connection():
     return pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=localhost;'
+        'SERVER=taskserver123.database.windows.net;'
         'DATABASE=TaskManager;'
-        'Trusted_Connection=yes;'
-
+        'UID=adminuser;'
+        'PWD=StrongPassword@123;'
+        'Encrypt=yes;'
+        'TrustServerCertificate=no;'
+        'Connection Timeout=30;'
     )
