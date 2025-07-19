b826d9dcecfc18e7a894f3af8b2eeadea7029af4
HarshAnand20102005 <pixelpunk45@gmail.com>
2025-07-19 19:57:11 +0530

    update2

--- Backend/db_connection.py
+++ Backend/db_connection.py
@@ -4,7 +4,7 @@ def get_connection():
     return pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
         'SERVER=taskserver123.database.windows.net;'
-        'DATABASE=task;'
+        'DATABASE=TaskManager;'
         'UID=adminuser;'
         'PWD=StrongPassword@123;'
         'Encrypt=yes;'
